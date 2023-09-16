81c6d69a5e53474786fc42e6a0079cb54158287a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-16 11:53:41 -0500

    feat: add catch all error boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AllPosts from '@/components/AllPosts';
 
 export default function Home() {
+  throw Error();
   return (
     <main className="flex flex-col items-center flex-1">
       <div className="flex flex-col w-full max-w-4xl gap-4">
